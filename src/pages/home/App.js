be67a0c931ef87e13b60250237556764d7842718
Gabriel de Carvalho Antunes <gabrielca.pro@gmail.com>
2026-01-12 13:21:52 -0300

    feat: background image

--- src/pages/home/App.js
+++ src/pages/home/App.js
@@ -1,10 +1,16 @@
 import { Header } from "../../components/Header"
+import background from '../../assets/background.jpg'
+import './styles.css'
+
 
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <div className='conteudo'>
+        <img src={background} className='background' alt='background'/>
+      </div>
     </div>
   );
 }
