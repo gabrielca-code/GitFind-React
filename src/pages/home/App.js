a96db793662c4b4acae9acc3469e49d8e8622b9c
Gabriel de Carvalho Antunes <gabrielca.pro@gmail.com>
2026-01-12 20:42:18 -0300

    feat: implementando fetch na api do github

--- src/pages/home/App.js
+++ src/pages/home/App.js
@@ -2,9 +2,23 @@ import { Header } from "../../components/Header"
 import background from '../../assets/background.jpg'
 import './styles.css'
 import ItemList from '../../components/ItemList'
+import { useState } from "react"
 
 
 function App() {
+  const [user, setUser] = useState('')
+  const [currentUser, setCurrentUser] = useState(null)
+  const [repos, setRepos] = useState(null)
+
+  const handleGetData = async () => {
+    console.log(user)
+    
+    const userData = await fetch(`https://api.github.com/users/${user}`)
+    const newUser = await userData.json()
+
+    console.log(newUser)
+  }
+
   return (
     <div className="App">
       <Header/>
@@ -13,8 +27,8 @@ function App() {
 
         <div className='info'>
           <div>  
-            <input name="usuario" placeholder="@username"/>
-            <button>Buscar</button>            
+            <input name="usuario" placeholder="@username" value={user} onChange={event => setUser(event.target.value)}/>
+            <button onClick={handleGetData}>Buscar</button>            
           </div>
 
           <div className="perfil">
