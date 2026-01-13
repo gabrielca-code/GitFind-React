1af0cb23249a9fec8d91db868fede3b7dbdcdca3
Gabriel de Carvalho Antunes <gabrielca.pro@gmail.com>
2026-01-13 15:24:24 -0300

    feat: consumindo a API para mostrar na tela

--- src/pages/home/App.js
+++ src/pages/home/App.js
@@ -16,7 +16,19 @@ function App() {
     const userData = await fetch(`https://api.github.com/users/${user}`)
     const newUser = await userData.json()
 
-    console.log(newUser)
+    if(newUser.name) {
+      const {avatar_url, name, bio, login} = newUser;
+      setCurrentUser({avatar_url, name, bio, login})
+    }
+
+    console.log(user)
+
+    const reposData = await fetch(`https://api.github.com/users/${user}/repos`)
+    const newRepos = await reposData.json()
+
+    if(newRepos.length) {
+      setRepos(newRepos)
+    }
   }
 
   return (
@@ -30,29 +42,32 @@ function App() {
             <input name="usuario" placeholder="@username" value={user} onChange={event => setUser(event.target.value)}/>
             <button onClick={handleGetData}>Buscar</button>            
           </div>
-
-          <div className="perfil">
-            <img src="https://avatars.githubusercontent.com/u/179767849?v=4" className="profile" alt="profile"/>
-            <div>
-              <h3>Nome perfil</h3>
-              <span>@gabriel-code</span>
-              <p>Descrição</p>
+          {currentUser?.name ? (
+            <>
+            <div className="perfil">
+              <img src={currentUser.avatar_url} className="profile" alt="profile"/>
+              <div>
+                <h3>{currentUser.name}</h3>
+                <span>@{currentUser.login}</span>
+                <p>{currentUser.bio}</p>
+              </div>
             </div>
-          </div>
 
-          <hr/>
+            </>
+          ) : null}
 
-          <div>
-            <h4 className="repositorio">Repositórios</h4>
-            <ItemList title="Titulo1" description="Descricao1"/>
-            <ItemList title="Titulo2" description="Descricao2"/>
-            <ItemList title="Titulo3" description="Descricao3"/>
-          </div>
+          {repos?.length ? (
+            <>
+            <hr/>
 
+            <div>
+              <h4 className="repositorio">Repositórios</h4>
+              {repos.map(repo => ( <ItemList title={repo.name} description={repo.description}/> ))}
+            </div>
+            </>
+          ) : null}
         </div>
       </div>
-
-      
     </div>
   );
 }
